976ed07a93ad0fcb3564c3387bf2be0edcac8d13
chaNcharge <chancharge@gmail.com>
2023-02-06 20:40:41 -0800

    Create register command, finish database creation

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 import time
 import json
 import sqlite3
+import asyncio
+import re
+import datetime
 
 from discord.ext import commands
 from discord.ext.commands import errors, Cog
@@ -13,36 +16,40 @@ def readConfig(file):
     try:
         with open(file, encoding='utf8') as data:
             return json.load(data, object_hook=lambda d: namedtuple("Config", d.keys())(*d.values()))
-    except AttributeError:
-        raise AttributeError("Unknown argument")
-    except FileNotFoundError:
-        raise FileNotFoundError("JSON file wasn't found")
+    except AttributeError as exc:
+        raise AttributeError("Unknown argument") from exc
+    except FileNotFoundError as exc:
+        raise FileNotFoundError("JSON file wasn't found") from exc
 
 
-# Config and bot object
-config = readConfig("config.json")
+# Initialization
+config = readConfig("config.json") # Config
 bot = commands.Bot(
     command_prefix=config.prefix, prefix=config.prefix, command_attrs=dict(
         hidden=True),
-    intents=discord.Intents(guilds=True, messages=True, members=True)
-)
+    intents=discord.Intents.all()
+) # Bot object
+con = sqlite3.connect("schedule.db") # We can set isolation_level to None for auto commit, but worse performance
+cur = con.cursor() # Use cur.execute("sql string") to execute sql on storage.db, then con.commit() when done
+
+# Table creation 
+# (Note that when we create new columns mid development, 
+# a new table will have to be created or an existing table manually edited)
+table_name = "schedule"
+cur.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+user_id INTEGER NOT NULL,
+name TEXT NOT NULL,
+birthday TIMESTAMP NOT NULL,
+has_role BOOLEAN NOT NULL,
+work_week TEXT,
+holiday TEXT,
+last_punch TIMESTAMP
+);""")
 
 
 class BossBot(Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        self.conn = sqlite3.connect("schedule.db")
-        self.cursor = self.conn.cursor()
-
-        self.cursor.excute("""
-            CREATE TABLE IF NOT EXIST schedule( 
-            user_id TEXT,
-            work_week TEXT,
-            holiday TEXT
-            )
-        """)
-
 
         print("Logging in...")
 
@@ -75,6 +82,11 @@ class BossBot(Cog):
         message = await ctx.send("Loading...")
         ping = int((time.monotonic() - before) * 1000)
         await message.edit(content=f"Pong! WS: {before_ws}ms  |  REST: {ping}ms")
+
+    @commands.command()
+    async def echo(self, ctx, toecho):
+        """ Repeats what is entered in echo command argument """
+        await ctx.send(toecho)
     
 
     @commands.Cog.listener()
@@ -84,5 +96,44 @@ class BossBot(Cog):
         if channel is not None:
             await channel.send(f"Hello! Welcome {member.mention} to the server!")
 
-bot.add_cog(BossBot(bot))
+    @commands.command()
+    async def register(self, ctx):
+        """ Register new user information. Starts multiple prompts. """
+        # Full name prompt
+        start_msg = await ctx.send(f"Hello there **{ctx.author.name}**, please enter your real first and last name.")
+        def check_name(m): # No requirements for name, whatever the user inputs, that's their name
+            return True
+        try:
+            userResponse = await self.bot.wait_for('message', timeout=30.0, check=check_name)
+        except asyncio.TimeoutError:
+            return await start_msg.edit(
+                content=f"~~{start_msg.clean_content}~~\n\nTimed out, please run register again."
+            )
+        name = userResponse.content
+        
+        # Birthday prompt
+        # Using RegEx to check if user input matches what is expected
+        re_timestamp = r"^(0[1-9]|1[0-2])\/(0[0-9]|1[0-9]|2[0-9]|3[0-1])\/([1-2]{1}[0-9]{3})"
+        birthday_msg = await ctx.send(f"Hello, **{name}**, now enter your date of birth in the following format `MM/DD/YYYY`")
+        def check_timestamp(m):
+            if (m.author == ctx.author and m.channel == ctx.channel):
+                if re.compile(re_timestamp).search(m.content):
+                    return True
+            return False
+
+        try:
+            userResponse = await self.bot.wait_for('message', timeout=30.0, check=check_timestamp)
+        except asyncio.TimeoutError:
+            return await birthday_msg.edit(
+                content=f"~~{start_msg.clean_content}~~\n\nTimed out or invalid inputs, please run register again."
+            )
+        date = datetime.datetime.strptime(userResponse.content.split(" ")[0], "%m/%d/%Y")
+        
+        cur.execute(f"""INSERT INTO {table_name} (user_id, name, birthday, has_role) 
+        VALUES ({ctx.author.id}, "{name}", "{date.strftime("%Y-%m-%d")} 00:00:00", False)""")
+        con.commit()
+
+        await ctx.send(f"Registered as {name} with birthday {date.strftime('%B %d, %Y')}.")
+
+asyncio.run(bot.add_cog(BossBot(bot)))
 bot.run(config.token)
