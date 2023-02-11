50e38546f5b7e4af46888838229d0a4aa2c83327
DJ-Willard <dwillar4@uoregon.edu>
2023-02-10 23:29:52 -0800

    files of function added

---
+++ dayQuote.py
@@ -0,0 +1,13 @@
+#quote_of_the_day
+import requests
+import json
+
+def get_quote_of_the_day():
+    response = requests.get("https://www.brainyquote.com/quote_of_the_day/qod.json")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        quote = data["contents"]["quotes"][0]["quote"]
+        print(quote)
+        return quote
+    else:
+        return "Sorry, I was unable to get the quote of the day."
