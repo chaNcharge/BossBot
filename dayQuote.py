8aeab7273f8eebcf4f5c7870c1341c7cf8882e77
DJ-Willard <dwillar4@uoregon.edu>
2023-03-09 08:56:10 -0800

    changed api to website for quote of the day

--- dayQuote.py
+++ dayQuote.py
@@ -1,12 +1,15 @@
 #quote_of_the_day
+#form They Said So Quotes API
+# referance document https://quotes.rest/
 import requests
 import json
 
 def get_quote_of_the_day():
-    response = requests.get("https://www.brainyquote.com/quote_of_the_day/qod.json")
+    response = requests.get("https://quotes.rest/qod")
+    data = response.json()
     if response.status_code == 200:
-        data = json.loads(response.text)
         quote = data["contents"]["quotes"][0]["quote"]
-        return quote
+        author = data["contents"]["quotes"][0]["author"]
+        print(f"\"{quote}\" - {author}")
     else:
         return "Sorry, I was unable to get the quote of the day."
